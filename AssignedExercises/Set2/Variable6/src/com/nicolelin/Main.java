a87d2d762a3cc7beca7f3b604d1b4575c7353cac
Nicole Lin <nicolelin2545@gmail.com>
2018-05-29 18:04:35 -0400

    finished variable6

---
+++ AssignedExercises/Set2/Variable6/src/com/nicolelin/Main.java
@@ -0,0 +1,19 @@
+package com.nicolelin;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int length;
+	int width;
+	Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter your length");
+        length = myScanner.nextInt();
+        System.out.println("Enter your width");
+        width = myScanner.nextInt();
+        int area = length * width;
+        int perimeter = length + length + width + width;
+        System.out.println("Your area is " + area + " and your perimeter is " + perimeter);
+
+    }
+}
---
+++ AssignedExercises/Set2/Variable7/src/com/nicolelin/Main.java
@@ -0,0 +1,8 @@
+package com.nicolelin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
