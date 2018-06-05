462d1b89c2eba09e01bd9901f777b0a170fd13b7
Nicole Lin <nicolelin2545@gmail.com>
2018-06-05 00:14:38 -0400

    finished repeat of set 3 except challenge and corrected string2 and 3

--- AssignedExercises/Set3/String2.1/src/com/nicolelin/Main.java
+++ AssignedExercises/Set3/String2.1/src/com/nicolelin/Main.java
@@ -7,8 +7,9 @@ public class Main {
 	    Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter your sentence");
         sentence = myScanner.nextLine();
-        String sentence.substring(0,1).toUpperCase();
-        System.out.println(sentence);
+        String letter1 = sentence.substring(0,1).toUpperCase();
+        String restletter = sentence.substring (1,sentence.length()).toLowerCase();
+        System.out.println(letter1 + restletter);
 
     }
 }
--- AssignedExercises/Set3/String3.1/src/com/nicolelin/Main.java
+++ AssignedExercises/Set3/String3.1/src/com/nicolelin/Main.java
@@ -8,16 +8,13 @@ public class Main {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter first sentence");
         sentence = myScanner.nextLine();
-        System.out.println(sentence.length());
         System.out.println("Enter second sentence");
         sentence2 = myScanner.nextLine();
-        System.out.println(sentence2.length());
 
-        if (sentence == sentence2){
-            System.out.println("0");
-        }else if (sentence == sentence2) {
-            System.out.println("Negative/positive");
-        }
+
+        int number = sentence.compareTo(sentence2);
+        System.out.println(number);
+
 
     }
 }
