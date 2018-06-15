5e0e9e8f1170e3f3de9bc4a1b9cf5c54971bbc6d
Nicole Lin <nicolelin2545@gmail.com>
2018-06-14 23:44:43 -0400

    continued selection9

--- AssignedExercises/Set4/Selection9/src/com/nicolelin/Main.java
+++ AssignedExercises/Set4/Selection9/src/com/nicolelin/Main.java
@@ -9,8 +9,33 @@ public class Main {
 	    String word3;
 	    String word4;
 	    String word5;
-        System.out.println("Enter 5 words");
+        System.out.println("Enter your first word");
+        word1 = myScanner.next();
+		System.out.println("Enter your second word");
+		word2 = myScanner.next();
+		System.out.println("Enter your third word");
+		word3 = myScanner.next();
+		System.out.println("Enter your fourth word");
+		word4 = myScanner.next();
+		System.out.println("Enter your last word");
+		word5 = myScanner.next();
+
+		word1.substring(0,1);
+		word2.substring(0,1);
+		word3.substring(0,1);
+		word4.substring(0,1);
+		word5.substring(0,1);
+
+		int var1 = word1.compareTo(word2);
+		
 
 
-    }
+
+
+
+
+
+
+
+	}
 }
