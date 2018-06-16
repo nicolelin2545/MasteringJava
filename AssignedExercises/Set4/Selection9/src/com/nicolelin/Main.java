cee4f50ea12ba7dd4dd2ab16dfa29d447bbc51eb
Nicole Lin <nicolelin2545@gmail.com>
2018-06-16 13:14:19 -0400

    progress selection9

--- AssignedExercises/Set4/Selection9/src/com/nicolelin/Main.java
+++ AssignedExercises/Set4/Selection9/src/com/nicolelin/Main.java
@@ -27,6 +27,22 @@ public class Main {
 		word5.substring(0,1);
 
 		int var1 = word1.compareTo(word2);
+		if (var1>0){
+			System.out.println(word1);
+		}else if (var1<0){
+			System.out.println(word2);
+		}else if (var1==0){
+			System.out.println(word1);
+		}
+		int var2 = word1.compareTo(word3);
+		if (var2>0){
+			System.out.println(word1);
+		}else if (var2<0){
+			System.out.println(word3);
+		}else if (var2==0){
+			System.out.println(word1);
+		}
+
 
 
 		// use  compare to and it will give integer value, use if statement to compare alphabetical letter
