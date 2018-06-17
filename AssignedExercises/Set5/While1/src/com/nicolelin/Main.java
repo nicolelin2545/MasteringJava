87f1af071fea9974680dfdba4a92e7c1e796f0ca
Nicole Lin <nicolelin2545@gmail.com>
2018-06-17 19:46:34 -0400

    finished while1

---
+++ AssignedExercises/Set5/While1/src/com/nicolelin/Main.java
@@ -0,0 +1,17 @@
+package com.nicolelin;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        String password = "Luby";
+        Scanner myScanner = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals(password)){
+            System.out.println("Enter password");
+            input = myScanner.nextLine();
+        }
+        System.out.println("You guessed the password");
+    }
+}
+
---
+++ AssignedExercises/Set5/WhileTestRun/src/com/nicolelin/Main.java
@@ -0,0 +1,12 @@
+package com.nicolelin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    while (){
+            System.out.println("Hello world");
+	    }
+
+    }
+}
