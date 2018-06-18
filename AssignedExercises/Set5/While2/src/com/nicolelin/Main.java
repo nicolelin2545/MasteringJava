5ddf6379f245c0c8d14d2047bf94116afad4e4ce
Nicole Lin <nicolelin2545@gmail.com>
2018-06-17 21:53:11 -0400

    tried while2

---
+++ AssignedExercises/Set5/While2/src/com/nicolelin/Main.java
@@ -0,0 +1,12 @@
+package com.nicolelin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double x = .5;
+	double y = Math.random();
+    }while (x!=y){
+        System.out.println("Re enter number");
+    }
+
+}
