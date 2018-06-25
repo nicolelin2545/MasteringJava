6f1c0cb7b227dc166fd30b78c1264ecfe8c42fbc
Nicole Lin <nicolelin2545@gmail.com>
2018-06-25 19:25:46 -0400

    updated while3

--- AssignedExercises/Set5/While3/src/com/nicolelin/Main.java
+++ AssignedExercises/Set5/While3/src/com/nicolelin/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	    int counter = 0;
 	    int number = 0;
-	    int sum = number;
 	    Scanner myScanner = new Scanner (System.in);
         System.out.println("Enter number");
         number = myScanner.nextInt();
 
         while (number!=0){
+            int sum = number;
             System.out.println("Re enter number");
             number = myScanner.nextInt();
             sum = sum + number;
