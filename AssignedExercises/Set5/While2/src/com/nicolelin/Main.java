3282e91d200e4d84ab40599cd3d01a9703523825
Nicole Lin <nicolelin2545@gmail.com>
2018-06-26 22:49:46 -0400

    attempted while2

--- AssignedExercises/Set5/While2/src/com/nicolelin/Main.java
+++ AssignedExercises/Set5/While2/src/com/nicolelin/Main.java
@@ -1,12 +1,25 @@
 package com.nicolelin;
-
+import java.util.Scanner;
+import java.lang.*;
 public class Main {
 
     public static void main(String[] args) {
-	double x = .5;
-	double y = Math.random();
-    }while (x!=y){
-        System.out.println("Re enter number");
+
+        double x=0;
+        double number = Math.random();
+        Scanner myScanner = new Scanner (System.in);
+
+        System.out.println("Enter number between 0.05 and 0.99");
+        number = myScanner.nextDouble();
+
+        while (x!=number){
+            System.out.println("Re enter number");
+            number = myScanner.nextDouble();
+        }
+        System.out.println("You guessed the right number");
+
+
+
     }
 
 }
