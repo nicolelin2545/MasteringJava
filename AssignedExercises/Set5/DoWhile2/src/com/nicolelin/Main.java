af3c2f4e4bf91f76371c43ed25f86d2be07866b8
Nicole Lin <nicolelin2545@gmail.com>
2018-07-09 23:12:07 -0400

    while2 almost complete

--- AssignedExercises/Set5/DoWhile2/src/com/nicolelin/Main.java
+++ AssignedExercises/Set5/DoWhile2/src/com/nicolelin/Main.java
@@ -3,14 +3,16 @@ import java.lang.*;
 public class Main {
 
     public static void main(String[] args) {
-        int a = (int) (Math.random() * (100 - 1) + 1);
-        int b = (int) (Math.random() * (100 - 1) + 1);
+        int a;
+        int b;
 
         do {
+            int a = (int) (Math.random() * (100 - 1) + 1);
+            int b = (int) (Math.random() * (100 - 1) + 1);
             System.out.println(a+b);
             System.out.println(a*b);
         } while (a + b != 20 && a * b != 96);
-        System.out.println("The two numbers are "+a "and " +b) ;
+        System.out.println("The two numbers are "+a + "and " +b) ;
 
     }
 }
