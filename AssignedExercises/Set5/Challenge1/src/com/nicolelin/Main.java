14816e09efc5c5d8ee4d8d391b5a2dff59821769
Nicole Lin <nicolelin2545@gmail.com>
2018-07-11 23:12:31 -0400

    attempted challenge 1

---
+++ AssignedExercises/Set5/Challenge1/src/com/nicolelin/Main.java
@@ -0,0 +1,24 @@
+package com.nicolelin;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	    int number;
+	    int input;
+	    Scanner myScanner = new Scanner (System.in);
+        System.out.println("Enter a number from 1-4");
+        input = myScanner.nextInt();
+        
+
+
+
+        while
+                (number!=input){
+            System.out.println();
+        }
+
+
+
+
+    }
+}
