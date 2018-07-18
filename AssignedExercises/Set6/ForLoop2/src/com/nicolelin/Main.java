1157a23d6b83eb7f93cbe3a94519e7b304642583
Nicole Lin <nicolelin2545@gmail.com>
2018-07-18 11:51:17 -0400

    finished for loop 2

---
+++ AssignedExercises/Set6/ForLoop2/src/com/nicolelin/Main.java
@@ -0,0 +1,10 @@
+package com.nicolelin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for (int counter = 0;counter<100;counter++){
+        System.out.println(counter);
+    }
+    }
+}
