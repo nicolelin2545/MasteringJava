0d834b9c5ac9e75baa676af238601217970c711c
Nicole Lin <nicolelin2545@gmail.com>
2018-07-17 23:43:51 -0400

    finished for loop 1

---
+++ AssignedExercises/Set6/untitled/src/com/nicolelin/Main.java
@@ -0,0 +1,12 @@
+package com.nicolelin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String word = "Nicole";
+	for (int c=0;c<10;c++){
+        System.out.println(word);
+    }
+
+    }
+}
