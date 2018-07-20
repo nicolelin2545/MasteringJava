ed411214f1e816de34aff80f6a0ffa0976b7821e
Nicole Lin <nicolelin2545@gmail.com>
2018-07-20 14:38:22 -0400

    for loop 3

--- AssignedExercises/Set6/ForLoop3/src/com/nicolelin/Main.java
+++ AssignedExercises/Set6/ForLoop3/src/com/nicolelin/Main.java
@@ -3,10 +3,12 @@ package com.nicolelin;
 public class Main {
 
     public static void main(String[] args) {
-	    for (int i=0;i<101;i++){
+	    for (int i=0;i<101;i++)
+	    {
             System.out.println(i);
         }
-        for (int i=100;i>-1;i--){
+        for (int i=100;i>=0;i--)
+        {
             System.out.println(i);
         }
     }
