7aff83300bdde54d5b90c00cfedf081850b9750a
Nicole Lin <nicolelin2545@gmail.com>
2018-07-23 22:11:32 -0400

    incomplete for loop 5

--- AssignedExercises/Set6/ForLoop5/src/com/nicolelin/Main.java
+++ AssignedExercises/Set6/ForLoop5/src/com/nicolelin/Main.java
@@ -1,8 +1,18 @@
 package com.nicolelin;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner myScanner = new Scanner (System.in);
+        System.out.println("Enter a word");
+        String word = myScanner.nextLine();
+        System.out.println("Enter how many times you want to print the word (a number)");
+        int num = myScanner.nextInt();
+
+
+        for (int i=0;i==num;){
+            System.out.println(word);
+        }
+
     }
 }
