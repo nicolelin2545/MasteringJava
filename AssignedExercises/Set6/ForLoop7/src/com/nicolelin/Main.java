dc09fcae1ab1b1916102f3d0bd2d04147a5bcf1d
Nicole Lin <nicolelin2545@gmail.com>
2018-07-26 22:07:00 -0400

    started for loop 7

---
+++ AssignedExercises/Set6/ForLoop7/src/com/nicolelin/Main.java
@@ -0,0 +1,9 @@
+package com.nicolelin;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner myScanner = new Scanner (System.in);
+        System.out.println("Enter 10 numbers");
+    }
+}
