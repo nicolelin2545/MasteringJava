643d64d0f3bd6004143a20796526afb10e0a88ec
Nicole Lin <nicolelin2545@gmail.com>
2018-07-28 22:45:35 -0400

    forloop7?

--- AssignedExercises/Set6/ForLoop7/src/com/nicolelin/Main.java
+++ AssignedExercises/Set6/ForLoop7/src/com/nicolelin/Main.java
@@ -4,6 +4,45 @@ public class Main {
 
     public static void main(String[] args) {
 	    Scanner myScanner = new Scanner (System.in);
-        System.out.println("Enter 10 numbers");
+
+        System.out.println("Enter first number");
+        int number1 = myScanner.nextInt();
+
+        System.out.println("Enter second number");
+        int number2 = myScanner.nextInt();
+
+        System.out.println("Enter third number");
+        int number3 = myScanner.nextInt();
+
+        System.out.println("Enter fourth number");
+        int number4 = myScanner.nextInt();
+
+        System.out.println("Enter fifth number");
+        int number5 = myScanner.nextInt();
+
+        System.out.println("Enter sixth number");
+        int number6 = myScanner.nextInt();
+
+        System.out.println("Enter seventh number");
+        int number7 = myScanner.nextInt();
+
+        System.out.println("Enter eighth number");
+        int number8 = myScanner.nextInt();
+
+        System.out.println("Enter ninth number");
+        int number9 = myScanner.nextInt();
+
+        System.out.println("Enter tenth number");
+        int number10 = myScanner.nextInt();
+
+
+
+        for (int i=0;i<number2+number2+number3+number4+number5+number6+number7+number8+number9+number10;i++){
+            System.out.println(i/10);
+        }
+
+
+
+
     }
 }
